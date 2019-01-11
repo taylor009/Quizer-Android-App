191e04fc6450d88a072a8666fbc4c4204b27dba7
taylor009 <taylor@baremetalcloud.com>
2019-01-11 17:24:16 -0500

    Cleaned up Activity Main Java file

--- app/src/main/java/com/tmgbinarycreations/quizzler/MainActivity.java
+++ app/src/main/java/com/tmgbinarycreations/quizzler/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -18,23 +19,27 @@ public class MainActivity extends Activity {
     // TODO: Declare member variables here:
      Button mTrueButton;
      Button mFalseButton;
+     TextView mQuestionTextView;
+     int mIndex;
+     int mQuestion;
+
 
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,27 +48,25 @@ public class MainActivity extends Activity {
 
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
+        mQuestionTextView = findViewById(R.id.question_text_view);
 
-        View.OnClickListener myListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("Quizzler", "Button pressed!");
-                Toast.makeText(getApplicationContext(), "True pressed!", Toast.LENGTH_SHORT).show();
-            }
-        };
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
 
-        mTrueButton.setOnClickListener(myListener);
+       mTrueButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Log.d("Quizzler", "Button pressed!");
+               Toast.makeText(getApplicationContext(), "True pressed!", Toast.LENGTH_SHORT).show();
+           }
+       });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast myToast = Toast.makeText(getApplicationContext(), "False pressed!", Toast.LENGTH_SHORT);
-                myToast.show();
-
-                TrueFalse exampleQuestion = new TrueFalse(R.string.question_1, true);
-
-                Random randomNumberGenerator = new Random();
+                Toast.makeText(getApplicationContext(), "False pressed!", Toast.LENGTH_SHORT).show();
             }
         });
+
 
     }
 }
